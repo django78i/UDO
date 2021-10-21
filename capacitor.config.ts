0dc5a6e57d8dc44e8044c93899b226082faced1c
Dame Cisse <elhdamecisse@gmail.com>
2021-10-21 19:09:07 +0200

    send dependencies

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,24 +1,43 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
+// @ts-ignore
 const config: CapacitorConfig = {
-  appId: 'com.UDO.app',
-  appName: 'UDOblank',
+  appId: 'fr.udo.health.blueprint',
+  appName: 'Udo',
   webDir: 'www',
   bundledWebRuntime: false,
-  "plugins": {
-		"SplashScreen": {
-			"launchShowDuration": 0
-		},
-		"GoogleAuth": {
-			"scopes": [
-				"profile",
-				"email"
-			],
-			"serverClientId": "911285735248-p38u0egepsqdnjc0stbmepg11g1cf4bc.apps.googleusercontent.com",
-			"forceCodeForRefreshToken": true
-		}
-	},
-
+  plugins: {
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    SplashScreen: {
+      launchShowDuration: 0
+    },
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    GoogleAuth: {
+      scopes: [
+        'profile',
+        'email'
+      ],
+      serverClientId: '911285735248-p38u0egepsqdnjc0stbmepg11g1cf4bc.apps.googleusercontent.com',
+      forceCodeForRefreshToken: true
+    }
+  },
+  cordova: {
+    preferences: {
+      ScrollEnabled: 'false',
+      BackupWebStorage: 'none',
+      SplashMaintainAspectRatio: 'true',
+      FadeSplashScreenDuration: '300',
+      SplashShowOnlyFirstTime: 'false',
+      SplashScreen: 'screen',
+      SplashScreenDelay: '3000'
+    },
+    plugins: {
+      'cordova-plugin-health': {
+        HEALTH_READ_PERMISSION: 'App needs read access',
+        HEALTH_WRITE_PERMISSION: 'App needs write access'
+      }
+    }
+  }
 };
 
 export default config;
