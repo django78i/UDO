1181acb938b8cd7de838cf125eb445c9786dcaca
niang.madjiguene94@gmail.com <niang.madjiguene94@gmail.com>
2021-10-23 20:48:31 +0000

    correction

--- src/app/session-now/demarrage/demarrage.page.ts
+++ src/app/session-now/demarrage/demarrage.page.ts
@@ -11,6 +11,7 @@ import { ReglagesPage } from '../reglages/reglages.page';
 import { Router } from '@angular/router';
 import { Health } from '@ionic-native/health/ngx';
 import { Platform } from '@ionic/angular';
+import { DonneesPriveComponent } from '../donnees-prive/donnees-prive.component';
 
 
 
@@ -20,224 +21,136 @@ import { Platform } from '@ionic/angular';
   styleUrls: ['./demarrage.page.scss'],
 })
 export class DemarragePage implements OnInit {
-  listElement: any = [];
-  listChoix = [
-    {
-      img: 'assets/images/distance.svg',
-      nombre: '1.78',
-      name: 'Distance',
-      exposant: 'KM',
-      fieldname:'distance'
-    },
-    {
-      img: 'assets/images/pas.svg',
-      nombre: '2617',
-      name: 'Nombre de pas',
-      exposant: '',
-      fieldname:'steps'
-    },
-    {
-      img: 'assets/images/coeur.svg',
-      nombre: '135',
-      name: 'BPM',
-      exposant: '',
-      fieldname:'height'
-    },
-    {
-      img: 'assets/images/calorie.svg',
-      nombre: '250',
-      name: 'Calories',
-      exposant: 'CAL',
-      fieldname:'weight'
-    },
-  ];
-  constructor(
-    private modalCtrl: ModalController,
-    private router: Router,
-    private toastCtrl: ToastController,
-    private camera: Camera,
-    public  navController: NavController,
-    private health: Health,
-    private platform: Platform
-  ) {}
+  listElement :any = [];
+  activite:any;
+  now;
+  pause:boolean = false;
+  listChoix = [{img:"assets/images/distance.svg",nombre:"1.78",name:"Distance" ,exposant:"KM"},{img:"assets/images/pas.svg",nombre:"2617",name:"Nombre de pas",exposant:""},{img:"assets/images/coeur.svg",nombre:"135",name:"BPM",exposant:""},{img:"assets/images/calorie.svg",nombre:"250",name:"Calories",exposant:"CAL"}];
+  constructor(private modalCtrl:ModalController,private router:Router,
+    private toastCtrl : ToastController, private camera:Camera) { }
 
   ngOnInit() {
-    this.checkPlatformReady();
-
-    this.listElement = [
-      {
-        img: 'assets/images/distance.svg',
-        nombre: '1.78',
-        name: 'Distance',
-        exposant: 'KM',
-        fieldname:'distance'
-      },
-      {
-        img: 'assets/images/pas.svg',
-        nombre: '2617',
-        name: 'Nombre de pas',
-        exposant: '',
-        fieldname:'steps'
-      },
-      {
-        img: 'assets/images/coeur.svg',
-        nombre: '135',
-        name: 'BPM',
-        exposant: '',
-        fieldname:'height'
-      },
-      {
-        img: 'assets/images/calorie.svg',
-        nombre: '250',
-        name: 'Calories',
-        exposant: 'CAL',
-        fieldname:'weight'
-      },
-    ];
+    let valPause = localStorage.getItem('pause');
+    if(valPause){
+      this.pause = true;
+    }
+    setInterval(() => {
+      this.now = new Date().toString().split(' ')[4].slice(0,5);
+    }, 1);
+    let item = JSON.parse(localStorage.getItem('activite'));
+    if(item){
+      this.activite = item;
+    }
+    this.listElement = [{img:"assets/images/distance.svg",nombre:"1.78",name:"Distance" ,exposant:"KM"},{img:"assets/images/pas.svg",nombre:"2617",name:"Nombre de pas",exposant:""},{img:"assets/images/coeur.svg",nombre:"135",name:"BPM",exposant:""},{img:"assets/images/calorie.svg",nombre:"250",name:"Calories",exposant:"CAL"}]
     let choix = localStorage.getItem('choix');
-    if (!choix) {
-      localStorage.setItem('choix', JSON.stringify(this.listChoix));
-    } else {
+    if(!choix){
+      localStorage.setItem('choix',JSON.stringify(this.listChoix));
+    }else{
       this.listElement = JSON.parse(localStorage.getItem('choix'));
     }
   }
-  async checkPlatformReady() {
-    const ready = !!await this.platform.ready();
-    if (ready) {
-// Use plugin functions here
-      this.health.requestAuthorization([
-        'distance', 'nutrition',  //read and write permissions
-        {
-          read: ['steps','height', 'weight'],//read only permission
-          write: ['height', 'weight']  //write only permission
-        }
-      ])
-        .then(res => this.getMetrics())
-        .catch(e => console.log('error1 '+e));
-    }
 
+  checkPause(){
+    this.pause=true;
+    localStorage.setItem('pause',''+this.pause);
   }
-  getMetrics(){
-    for (let item of this.listElement){
-     this.queryMetrics(item.fieldname,item);
-    }
-    let that=this;
-    // this.getMetrics();
-    // @ts-ignore
-     setTimeout(()=>{that.getMetrics();},1000);
+
+ async publier(){
+    const modal = await this.modalCtrl.create({
+      component: DonneesPriveComponent,
+      cssClass: 'my-custom-contenu-modal',
+    });
+    return await modal.present();
   }
-  // @ts-ignore
-  queryMetrics(metric,item){
-    if (metric==='steps' || metric==='distance' ){
-      this.health.queryAggregated({
-        startDate: new Date(new Date().getTime() - 3 * 24 * 60 * 60 * 1000), // three days ago
-        endDate: new Date(), // now
-        dataType: metric,
-        bucket:'day',
-        //limit: 1000
-      }).then(res=>{
-        item.nombre=res.length>0?(Math.round((parseFloat(res[res.length-1]?.value) + Number.EPSILON) * 100) / 100):'0';
-        console.log('res',res);
-      })
-        .catch(e => console.log('error3 ', e));
-    }
-    else{
-      this.health.query({
-        startDate: new Date(new Date().getTime() - 10 * 24 * 60 * 60 * 1000), // three days ago
-        endDate: new Date(), // now
-        dataType: metric,
-        limit: 100
-      }).then(res=>{
-        item.nombre=res.length>0?(Math.round((parseFloat(res[res.length-1]?.value) + Number.EPSILON) * 100) / 100):'0';
-        console.log('res',res);})
-        .catch(e => console.log('error1 ',e));
-    }
-  }
-  async choix(item, index) {
+
+  async choix(item,index) {
     const modal = await this.modalCtrl.create({
       component: ListMetricsPage,
       cssClass: 'my-custom-activite-modal',
       componentProps: {
-        choix: item,
-      },
+        choix:item
+      }
     });
     modal.onDidDismiss().then((data: any) => {
-      if (data.data) {
+      if(data.data){
         let value = data.data;
-        for (let el of this.listElement) {
-          if (el.name == value.name) {
-            this.showMessage(
-              'Cette activité est déjà dans la liste',
-              'warning'
-            );
+        for(let el of this.listElement){
+          if(el.name == value.name){
+            this.showMessage('Cette activité est déjà dans la liste','warning')
             return;
           }
+          
         }
-        for (let val of this.listChoix) {
-          if (value.name == val.name) {
+        for(let val of this.listChoix){
+          if(value.name == val.name){
             value = val;
-            this.listElement[index] = val;
-          } else {
-            this.listElement[index] = value;
+            this.listElement[index]=val;
+          }else{
+            this.listElement[index]=value;
           }
         }
-        localStorage.setItem('choix', JSON.stringify(this.listElement));
+        localStorage.setItem('choix',JSON.stringify(this.listElement));
       }
     });
     return await modal.present();
+
   }
+  
   async showMessage(message, type) {
     const toast = await this.toastCtrl.create({
-      message,
-      duration: 4000,
-      color: type,
-      position: 'bottom',
-    });
+         message,
+         duration: 4000,
+         color: type,
+         position:'bottom' 
+       });
     toast.present();
   }
-  information() {
-    this.router.navigate([['session-now/aide']]);
+ information() {
+    this.router.navigate(['aide']);
   }
 
   async notification() {
     const modal = await this.modalCtrl.create({
       component: NotificationsPage,
       cssClass: 'my-custom-activite-modal',
-      componentProps: {},
+      componentProps: {
+
+      }
     });
-    modal.onDidDismiss().then((data: any) => {});
+    modal.onDidDismiss().then((data: any) => {
+      
+    });
     return await modal.present();
+
   }
-  openCamera() {
+  openCamera(){
     const options: CameraOptions = {
       quality: 100,
       destinationType: this.camera.DestinationType.FILE_URI,
       encodingType: this.camera.EncodingType.JPEG,
-      mediaType: this.camera.MediaType.PICTURE,
-    };
-
-    this.camera.getPicture(options).then(
-      (imageData) => {
-        // imageData is either a base64 encoded string or a file URI
-        // If it's base64 (DATA_URL):
-        let base64Image = 'data:image/jpeg;base64,' + imageData;
-      },
-      (err) => {
-        // Handle error
-      }
-    );
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    
+    this.camera.getPicture(options).then((imageData) => {
+     // imageData is either a base64 encoded string or a file URI
+     // If it's base64 (DATA_URL):
+     let base64Image = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+     // Handle error
+    });
   }
   async reglage() {
     const modal = await this.modalCtrl.create({
       component: ReglagesPage,
       cssClass: 'my-custom-activite-modal',
-      componentProps: {},
-    });
-    modal.onDidDismiss().then((data: any) => {});
-    return await modal.present();
-  }
+      componentProps: {
 
-  back() {
-    this.navController.navigateForward(['tabs/tab3']);
+      }
+    });
+    modal.onDidDismiss().then((data: any) => {
+      
+    });
+    return await modal.present();
+
   }
 }
